b9a2adb199635a38d811140e227550eb2a945c6f
naviadev <qo2oq1020@gmail.com>
2024-06-11 10:55:25 +0900

    캡슐화의 필요성, 객체를 동적할당하는 작업을 수행

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+ const userInfo = (name, age, job) => {
+  
+  return {
+    name : name,
+    age : age ,
+    job : job,
+  }
+
+};
+
+const person = userInfo('배성빈', 1, '학생');
+
+person.address = '대전'
+
+console.log(person);
+
